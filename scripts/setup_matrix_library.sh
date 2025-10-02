be499394c0219706b9cfdd95376770356bb7e92e
Benjamin Wiebe <ben@wiebe.email>
2025-10-01 21:34:35 -0400

    Fix RGB Matrix setup

--- scripts/setup_matrix_library.sh
+++ scripts/setup_matrix_library.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+repodir=$(pwd)
+
 cd $GOPATH/pkg/mod/github.com/tfk1410/go-rpi-rgb-led-matrix@v0.0.0-20210404121211-ed43f29cbccb
 chmod 700 .
 mkdir vendor
 cd vendor
 git clone https://github.com/hzeller/rpi-rgb-led-matrix.git
 cd rpi-rgb-led-matrix
-#sed -i '' 's/.*RGB_SLOWDOWN_GPIO.*/DEFINE+=-DRGB_SLOWDOWN_GPIO=2/' lib/Makefile
+sed -i '' 's/.*RGB_SLOWDOWN_GPIO.*/DEFINE+=-DRGB_SLOWDOWN_GPIO=2/' lib/Makefile
 make -j
+
+cd $repodir
+go get -v github.com/tfk1410/go-rpi-rgb-led-matrix
